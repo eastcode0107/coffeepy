72b94fdd9dacdfea0493d30c49d10780b1efa978
eastcode0107 <144340363+eastcode0107@users.noreply.github.com>
2023-09-08 14:48:57 +0000

    Improvements
    Changes:
    1. Added a while loop in main function to better handle the user choices, especially exiting.
    2. Maid a resource dictionary to keep track of resources.
    3. Added a for loop sin buy function to handle buying of different products instead of having multiple if else for different products.
    4. Minor changes to prints trying to get used to \n

--- main.py
+++ main.py
@@ -1,138 +1,94 @@
-import sys
+class CoffeeMachine:
 
-# Coffee Mach State
-mach_water = 400
-mach_milk = 540
-mach_beans = 120
-mach_cups = 9
-mach_money = 550
+  def __init__(self):
+    self.resources = {
+        "water": 400,
+        "milk": 540,
+        "beans": 120,
+        "cups": 9,
+        "money": 550
+    }
 
-def buy(mach_water, mach_milk, mach_beans, mach_cups, mach_money):
-  print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-  choice = input()
-  print("")
-  if choice == "1":  
-    water_need = 250
-    beans_need = 16
-    if mach_water >= water_need and mach_beans >= beans_need and mach_cups >= 1:
-      mach_water -= water_need
-      mach_beans -= beans_need
-      mach_cups -= 1
-      mach_money += 4
-      print("I have enough resources, making you a coffee!")
-      action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-    elif mach_water < water_need:
-      print("Sorry, not enough water!")
-    elif mach_beans < beans_need:
-      print("Sorry, not enough beans!")
-    elif mach_cups < 1:
-      print("Sorry, not enough cups!")
-    print("")
-    action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-  if choice == "2":
-    water_need = 350
-    milk_need = 75
-    beans_need = 20
-    if (mach_water >= water_need and mach_milk >= milk_need
-        and mach_beans >= beans_need and mach_cups >= 1):
-      mach_water -= water_need
-      mach_milk -= milk_need
-      mach_beans -= beans_need
-      mach_cups -= 1
-      mach_money += 7
-      print("I have enough resources, making you a coffee!")
-      action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-    elif mach_water < water_need:
-      print("Sorry, not enough water!")
-    elif mach_milk < milk_need:
-      print("Sorry, not enough milk!")
-    elif mach_beans < beans_need:
-      print("Sorry, not enough beans!")
-    elif mach_cups < 1:
-      print("Sorry, not enough cups!")
-    print("")
-    action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-  if choice == "3":
-    water_need = 200
-    milk_need = 100
-    beans_need = 12
-    if (mach_water >= water_need and mach_milk >= milk_need
-        and mach_beans >= beans_need and mach_cups >= 1):
-      mach_water -= water_need
-      mach_milk -= milk_need
-      mach_beans -= beans_need
-      mach_cups -= 1
-      mach_money += 6
-      print("I have enough resources, making you a coffee!")
-      action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-    elif mach_water < water_need:
-      print("Sorry, not enough water!")
-    elif mach_milk < milk_need:
-      print("Sorry, not enough milk!")
-    elif mach_beans < beans_need:
-      print("Sorry, not enough beans!")
-    elif mach_cups < 1:
-      print("Sorry, not enough cups!")
-    print("")
-    action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-  if choice == "back":
-    print("")
-    action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
+  def main(self):
+    while True:
+      user_choice = input("Write action (buy, fill, take, remaining, exit):\n")
+      print("")
+      if user_choice == "exit":
+        break
+      else:
+        self.action(user_choice)
+
+  def action(self, action):
+    if action == "buy":
+      self.buy()
+    elif action == "fill":
+      self.fill()
+    elif action == "take":
+      self.take()
+    elif action == "remaining":
+      self.status()
+
+  def buy(self):
+    choices = {
+        "1": {
+            "water": 250,
+            "beans": 16,
+            "cups": 1,
+            "money": -4
+        },
+        "2": {
+            "water": 350,
+            "milk": 75,
+            "beans": 20,
+            "cups": 1,
+            "money": -7
+        },
+        "3": {
+            "water": 200,
+            "milk": 100,
+            "beans": 12,
+            "cups": 1,
+            "money": -6
+        }
+    }
+    user_input = input(
+"What do you want to buy? 1 - espresso,2 - latte, 3 - cappuccino, back - to main menu:\n"
+    )
+    if user_input == "back":
+      return
+    else:
+      for resource, quantity in choices[user_input].items():
+        if self.resources[resource] - quantity < 0:
+          print("Sorry, not enough {}!\n".format(resource))
+          return
+        
+      print("I have enough resources, making you a coffee!\n")
+      for resource, quantity in choices[user_input].items():
+        self.resources[resource] -= quantity
+
+  def fill(self):
+    self.resources["water"] += int(
+        input("Write how many ml of water you want to add:\n"))
+    self.resources["milk"] += int(
+        input("Write how many ml of milk you want to add:\n"))
+    self.resources["beans"] += int(
+        input("Write how many grams of coffee beans you want to add:\n"))
+    self.resources["cups"] += int(
+        input("Write how many disposable cups you want to add:\n"))
+
+  def take(self):
+    print("I gave you ${}".format(self.resources['money']))
+    self.resources['money'] = 0
     
-def fill(mach_water, mach_milk, mach_beans, mach_cups, mach_money):
-  print("Write how many ml of water you want to add: ")
-  add_water = int(input())
-  mach_water += add_water
-  print("Write how many ml of milk you want to add: ")
-  add_milk = int(input())
-  mach_milk += add_milk
-  print("Write how many grams of coffee beans you want to add")
-  add_beans = int(input())
-  mach_beans += add_beans
-  print("Write how many disposable cups you want to add: ")
-  add_cups = int(input())
-  mach_cups += add_cups
-  print("")
-  action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-def take(mach_water, mach_milk, mach_beans, mach_cups, mach_money):
-  print("I gave you ${}".format(mach_money))
-  mach_money = 0
-  print("")
-  action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-def status(mach_water, mach_milk, mach_beans, mach_cups, mach_money):
-  print("The coffee machine has:")
-  print("{} ml of water".format(mach_water))
-  print("{} ml of milk".format(mach_milk))
-  print("{} g of coffee beans".format(mach_beans))
-  print("{} disposable cups".format(mach_cups))
-  print("${} of money".format(mach_money))
-  print("")
-  action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-def action(mach_water, mach_milk, mach_beans, mach_cups, mach_money):
-  print("Write action (buy, fill, take, remaining, exit): ")
-  action = input()
-  if action == "buy":
-    buy(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-  elif action == "fill":
-    fill(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-  elif action == "take":
-    take(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-
-  elif action == "remaining":
-    status(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
-    
-  elif action == "exit":
-    sys.exit()
-
-def main():
-  # Start
-  # commit test 
-  action(mach_water, mach_milk, mach_beans, mach_cups, mach_money)
+  def status(self):
+    print("The coffee machine has:")
+    print("{} ml of water".format(self.resources['water']))
+    print("{} ml of milk".format(self.resources['milk']))
+    print("{} g of coffee beans".format(self.resources['beans']))
+    print("{} disposable cups".format(self.resources['cups']))
+    print("${} of money".format(self.resources['money']))
+    print("")
+    return
 
 if __name__ == "__main__":
-  main()
+  CoffeeMachine().main()
